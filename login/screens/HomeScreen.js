e8e034728cdb0b531caad0e5e05dfa658270ad6c
unknown <sandesnaga@yahoo.com>
2019-09-06 14:05:26 -0500

    home screen refactor

--- login/screens/HomeScreen.js
+++ login/screens/HomeScreen.js
@@ -1,13 +1,71 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-
+import { StyleSheet, Text, View, Image } from 'react-native';
+import * as firebase from "firebase";
+import {Button} from "native-base"
 
 export default class HomeScreen extends React.Component{
-  render(){
+  constructor(props){
+    super(props);
+    this.state = {
+      name:"",
+      email: ""
+    }
+  }
+  
+  static navigationOptions = {
+    title: "Home",
+    header: null
+}
+
+componentDidMount(){
+  firebase.auth().onAuthStateChanged(authenticate => {
+    if(authenticate){
+      this.setState({
+        email: authenticate.email,
+        name: authenticate.displayName
+      })
+    }
+    else{
+      this.props.navigation.replace("SignIn");
+    }
+  })
+}
+
+signOutUser = ( ) =>{
+  firebase
+  .auth()
+  .signOut()
+  .then(() => {console.log("signout")})
+  .catch( error => {
+    alert(error.message);
+  })
+}
+
+
+render(){
     return (
       <View style={styles.container}>
-        <Text>HomeScreen</Text>
+        <View style={styles.logoconainer}>
+        <Image
+          style={{width:  160, height:80}}
+          source={require("../assets/logo.png")}
+          />
+        </View>
+        <View style={styles.userDetails}>
+          <Text> Hey {this.state.name}</Text>
+          <Text> You are signed in as:  {this.state.email}</Text>
+        </View>
+        <Button
+        style={styles.button}
+        full 
+        rounded
+        success
+        onPress={()=>{
+
+        }}
+        >
+          <Text style={styles.buttonText}>Sign Out</Text>
+        </Button>
       </View>
     );
   }
@@ -17,8 +75,21 @@ export default class HomeScreen extends React.Component{
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    margin: 20
   },
+  logoContainer: {
+    alignItems: "center",
+    marginTop: 100,
+    marginBottom: 100
+  },
+  userDetails: {},
+
+  button: {
+    marginTop: 20
+  },
+  buttonText: {
+    color: "#fff"
+  }
 });
